c02970d316091cbbb6a313898c09f1d343373251
StarNumber12046 <64470722+StarNumber12046@users.noreply.github.com>
2024-12-04 19:38:46 +0100

    Add project files.

---
+++ CosmeticUtils/Commands.cs
@@ -0,0 +1,71 @@
+﻿using MIU;
+using UnityEngine;
+
+namespace CosmeticUtils
+{
+    internal class Commands
+    {
+        [ConsoleCommand(null, null, null, false, false, description = "Search for a cosmetic and return its info")]
+        public static string searchCosmetic(params string[] name)
+        {
+            if (name.Length == 0) return "Please specify a falue for parameter 'name'";
+            foreach (var k in CosmeticManager.instance.cosmetics.Skins)
+            {
+                if (k.name.ToLower().Contains(name[0].ToLower())) MonoBehaviour.print($"{k.name} ({k.Id}) - skin");
+            }
+            foreach (var k in CosmeticManager.instance.cosmetics.Trails)
+            {
+                if (k.name.ToLower().Contains(name[0].ToLower())) MonoBehaviour.print($"{k.name} ({k.Id}) - trail");
+            }
+            foreach (var k in CosmeticManager.instance.cosmetics.Hats)
+            {
+                if (k.name.ToLower().Contains(name[0].ToLower())) MonoBehaviour.print($"{k.name} ({k.Id}) - hat");
+            }
+            foreach (var k in CosmeticManager.instance.cosmetics.Respawns)
+            {
+                if (k.name.ToLower().Contains(name[0].ToLower())) MonoBehaviour.print($"{k.name} ({k.Id}) - respawn effect");
+            }
+            foreach (var k in CosmeticManager.instance.cosmetics.Blasts)
+            {
+                if (k.name.ToLower().Contains(name[0].ToLower())) MonoBehaviour.print($"{k.name} ({k.Id}) - blast");
+            }
+            return "";
+        }
+
+        [ConsoleCommand(null, null, null, false, false, description = "Search for a cosmetic and return its info")]
+        public static string getCosmetics(params string[] type)
+        {
+            if (type.Length == 0) return "Please specify a falue for parameter 'type'";
+            Cosmetic[] cosmetics = null;
+            switch (type[0])
+            {
+                case "marbles":
+                    {
+                        cosmetics = CosmeticManager.instance.cosmetics.Skins; break;
+                    }
+                case "trails":
+                    {
+                        cosmetics = CosmeticManager.instance.cosmetics.Trails; break;
+                    }
+                case "hats":
+                    {
+                        cosmetics = CosmeticManager.instance.cosmetics.Hats; break;
+                    }
+                case "respawns":
+                    {
+                        cosmetics = CosmeticManager.instance.cosmetics.Respawns; break;
+                    }
+                case "blasts":
+                    {
+                        cosmetics = CosmeticManager.instance.cosmetics.Blasts; break;
+                    }
+                default: return "Value for parameter 'type' is incorrect. Choices: [marbles/trails/hats/respawns/blasts]";
+            }
+            foreach (var k in cosmetics)
+            {
+                MonoBehaviour.print($"{k.name} ({k.Id})");
+            }
+            return "";
+        }
+    }
+}
---
+++ CosmeticUtils/OnLoad.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using HarmonyLib;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using System.Reflection;
+using MIU;
+
+
+namespace CosmeticUtils
+{
+    [HarmonyPatch(typeof(PlatformSetup), "GetDevIDs")] // Here before ConsoleUnlocker gets ported
+    internal class DevIdPatches
+    {
+        public static void Postfix()
+        {
+            FieldInfo DevsField = typeof(PlatformSetup).GetField("Devs", BindingFlags.NonPublic | BindingFlags.Static);
+            if (DevsField == null) return;
+            List<DevUser> Devs = DevsField.GetValue(null) as List<DevUser>;
+            DevUser me = new DevUser($"{{\"UserID\": \"{Player.Current.Network.GetNetworkId()}\", \"Flags\": 31}}");
+            MonoBehaviour.print($"Me (DevUser) | IsDev: {me.HasFeature(1)} | UnlockAll:  {me.HasFeature(2)} | MultiplayerFlag: {me.HasFeature(4)} | CommandLineAccess: {me.HasFeature(8)} | FPSDisplay: {me.HasFeature(16)}");
+            Devs.Add(me);
+            DevsField.SetValue(null, Devs);
+            GlobalContext.Invoke<DevModeActivated>(Array.Empty<object>());
+            MIU.Console.Instance.gameObject.SetActive(true);
+        }
+    }
+
+    public class CrystalMarble
+    {
+        public static bool Patched = false;
+
+        public static void OnLoad()
+        {
+            SceneManager.sceneLoaded += new UnityAction<Scene, LoadSceneMode>(CrystalMarble.Patch);
+        }
+        public static void Patch(Scene scene, LoadSceneMode lsm)
+        {
+            if (!CrystalMarble.Patched)
+            {
+                new Harmony("com.starnumber.cosmeticutils").PatchAll();
+                CrystalMarble.Patched = false;
+                SceneManager.sceneLoaded -= new UnityAction<Scene, LoadSceneMode>(CrystalMarble.Patch);
+            }
+        }
+    }
+}
